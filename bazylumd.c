df6c7cf6d8d3a3ea01b0e0a3e5a5ede9cca9686e
Roman Bogorodskiy <bogorodskiy@gmail.com>
2009-12-26 12:46:22 +0300

    Split stuff into functions.

--- bazylumd.c
+++ bazylumd.c
@@ -18,24 +18,77 @@ void log_window_activity(sqlite3 *db, char *window_name, int timeout)
 	sqlite3_free(query);
 }
 
+sqlite3* init_database()
+{
+	int rc;
+	sqlite3 *db;
+
+	rc = sqlite3_open(DATABASE, &db);
+
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+		sqlite3_close(db);
+		exit(1);
+	}
+
+	return db;
+}
+
+char* x11_get_active_window_name(Display *dpy, Window root_window)
+{
+	Atom actual_type;
+	Atom *props;
+	int actual_format, status;
+	long nitems, bytes;
+	long *data;
+	Window active_window;
+	int n;
+
+	status = XGetWindowProperty(
+		dpy,
+		root_window,
+		XInternAtom(dpy, "_NET_ACTIVE_WINDOW", True),
+		0,
+		(~0L),
+		False,
+		AnyPropertyType,
+		&actual_type,
+		&actual_format,
+		&nitems,
+		&bytes,
+		(unsigned char**)&data);
+
+	active_window = data[0];              
+
+	props = XListProperties(dpy, active_window, &n);
+
+	status = XGetWindowProperty(
+		dpy,
+		active_window,
+		XInternAtom(dpy, "WM_CLASS", True),
+		0,
+		(~0L),
+		False,
+		AnyPropertyType,
+		&actual_type,
+		&actual_format,
+		&nitems,
+		&bytes,
+		(unsigned char**)&data);
+
+	return (char *)data;
+}
+
 int main(int argc, char **argv)
 {
 	Display *dpy;
 	int screen;
-	Atom     actual_type;
-	Atom *props;
-	int      actual_format;
-	long     nitems;
-	long     bytes;
-	long     *data;
 	char *prev_window = NULL;
-	int      status;
+	char *cur_window;
 	int i, n;
-	Window active_window;
 	int timeout = 1;
 	int active_time = 0;
 	sqlite3 *db;
-	int rc;
 	int opt;
 	short foreground = 0;
 	pid_t pid, sid;
@@ -75,70 +128,22 @@ int main(int argc, char **argv)
 	w = RootWindow(dpy, screen);
 
 	/* init database connection */
-	rc = sqlite3_open(DATABASE, &db);
-
-	if (rc != SQLITE_OK) {
-		fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
-		sqlite3_close(db);
-		exit(1);
-	}
+	db = init_database();
 
 	for (;;) {
-		status = XGetWindowProperty(
-			dpy,
-			w,
-			XInternAtom(dpy, "_NET_ACTIVE_WINDOW", True),
-			0,
-			(~0L),
-			False,
-			AnyPropertyType,
-			&actual_type,
-			&actual_format,
-			&nitems,
-			&bytes,
-			(char**)&data);
-
-
-	//	printf("hello world\n");
-	//	 for (i=0; i < nitems; i++){
-	  //              printf("data[%d] %d\n", i, data[i]);
-		active_window = data[0];              
-
-		props = XListProperties(dpy, active_window, &n);
-	//	printf("n = %d\n", n);
-
-	//	for (i = 0; i < n; i++) {
-	//		printf("%s\n", XGetAtomName(dpy, props[i]));
-	//	}
-	//        }
-
-		status = XGetWindowProperty(
-			dpy,
-			active_window,
-			XInternAtom(dpy, "WM_CLASS", True),
-			0,
-			(~0L),
-			False,
-			AnyPropertyType,
-			&actual_type,
-			&actual_format,
-			&nitems,
-			&bytes,
-			(char**)&data);
+		cur_window = x11_get_active_window_name(dpy, w);
 
 		if (prev_window != NULL) {
-			if (strcmp(data, prev_window) == 0) {
+			if (strcmp(cur_window, prev_window) == 0) {
 				active_time += timeout;
 			} else {
-				//printf("%s has been active for %d seconds\n", prev_window, active_time);
 				if (active_time != 0)
 					log_window_activity(db, prev_window, active_time);
 				active_time = 0;
 			}
 		}
 
-		//printf("current: %s, previous: %s\n", data, prev_window);
-		prev_window = strdup(data);
+		prev_window = strdup(cur_window);
 
 		sleep(timeout);
 	}
