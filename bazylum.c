40c6d0fd59d392d26147418e58cbb364834526d4
Roman Bogorodskiy <bogorodskiy@gmail.com>
2009-12-28 00:25:09 +0300

    Initial implementation fo the 'stat' command for client.

--- bazylum.c
+++ bazylum.c
@@ -1,9 +1,19 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sqlite3.h>
 
 #define	DATABASE	"bazylum.db"
+
+static int stat_callback(void *argno, int argc, char **argv, char **colname)
+{
+	assert(argc == 2);
+
+	printf("%s: %d sec\n", argv[0], atoi(argv[1]));
+	return 0;
+}
 
 sqlite3 *open_database()
 {
@@ -24,10 +34,20 @@ sqlite3 *open_database()
 void do_stat()
 {
 	sqlite3 *db;
+	int rc;
+	char *err;
 
+	/* should be moved to main() when have more commands */
 	db = open_database();
 
-	printf("stat\n");
+	rc = sqlite3_exec(db, "SELECT window_name,SUM(window_time) AS total_time FROM bazylum "
+			"GROUP BY window_name ORDER BY total_time DESC", stat_callback,
+			0, &err);
+	if (SQLITE_OK != rc) {
+		fprintf(stderr, "Error executing query: %s\n", err);
+		sqlite3_free(err);
+		exit(1);
+	}
 }
 
 int main(int argc, char **argv)
